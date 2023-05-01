89a5009a7951cb4e5337387f9f87ebd95bd4f0e2
RA <numpde@null.net>
2023-05-01 23:28:45 +0400

    more tests

--- bike_nft/test/BicycleComponentV1.ts
+++ bike_nft/test/BicycleComponentV1.ts
@@ -5,7 +5,7 @@ import {ethers} from "hardhat";
 
 describe("BicycleComponentV1", function () {
     async function deployBicycleComponentFixture() {
-        const [owner, other] = await ethers.getSigners();
+        const [owner, other, third] = await ethers.getSigners();
 
         const BicycleComponentV1 = await ethers.getContractFactory("BicycleComponentV1");
 
@@ -16,21 +16,22 @@ describe("BicycleComponentV1", function () {
             value: 0
         });
 
-        return {bicycleComponentV1, owner, other};
+        return {bicycleComponentV1, owner, other, third};
     }
 
 
     async function registerComponent() {
-        const {bicycleComponentV1, owner, other} = await loadFixture(deployBicycleComponentFixture);
+        const {bicycleComponentV1, owner: shop, other: customer, third: third} = await loadFixture(deployBicycleComponentFixture);
 
         const serialNumber = "SN12345678";
         const uri = "https://example.com/" + serialNumber;
 
         const tokenId = await bicycleComponentV1.generateTokenId(serialNumber);
 
-        await bicycleComponentV1.register(other.address, serialNumber, uri);
+        await bicycleComponentV1.register(customer.address, serialNumber, uri);
 
-        return {bicycleComponentV1, sender: owner, customer: other, serialNumber, uri, tokenId};
+        // todo: check syntax
+        return {bicycleComponentV1, shop, customer, third, serialNumber, uri, tokenId};
     }
 
 
@@ -69,7 +70,6 @@ describe("BicycleComponentV1", function () {
 
 
         describe("Registration", function () {
-
             it("Should assign initial ownership correctly", async function () {
                 const {bicycleComponentV1, customer, tokenId} = await loadFixture(registerComponent);
 
@@ -79,7 +79,7 @@ describe("BicycleComponentV1", function () {
             });
 
             it("Should assign initial URI correctly", async function () {
-                const {bicycleComponentV1, owner, other, serialNumber, uri} = await loadFixture(registerComponent);
+                const {bicycleComponentV1, serialNumber, uri} = await loadFixture(registerComponent);
 
                 const tokenId = await bicycleComponentV1.generateTokenId(serialNumber);
                 const tokenURI = await bicycleComponentV1.tokenURI(tokenId);
@@ -88,10 +88,9 @@ describe("BicycleComponentV1", function () {
             });
 
             it("Should approve sender as operator for the newly minted token", async function () {
-                const {bicycleComponentV1, sender, serialNumber} = await loadFixture(registerComponent);
+                const {bicycleComponentV1, shop, tokenId} = await loadFixture(registerComponent);
 
-                const tokenId = await bicycleComponentV1.generateTokenId(serialNumber);
-                const isApproved = await bicycleComponentV1.tokenOperatorApproval(tokenId, sender.address);
+                const isApproved = await bicycleComponentV1.tokenOperatorApproval(tokenId, shop.address);
 
                 expect(isApproved).to.be.true;
             });
@@ -129,12 +128,156 @@ describe("BicycleComponentV1", function () {
             });
         });
 
-        describe("_isApprovedOrOwner", function () {
+        describe("setTokenOperatorApproval", function () {
+            it("Should set approval for a given operator and tokenId", async function () {
+                const {bicycleComponentV1, tokenId, third} = await loadFixture(registerComponent);
+
+                const isApprovedBefore = await bicycleComponentV1.tokenOperatorApproval(tokenId, third.address);
+
+                await bicycleComponentV1.setTokenOperatorApproval(tokenId, third.address, true);
+
+                const isApprovedAfter = await bicycleComponentV1.tokenOperatorApproval(tokenId, third.address);
+
+                expect(isApprovedBefore).to.be.false;
+                expect(isApprovedAfter).to.be.true;
+            });
+
+            it("Should emit TokenOperatorApprovalUpdated event when approval is granted", async function () {
+                const {bicycleComponentV1, tokenId, third} = await loadFixture(registerComponent);
+
+                await expect(bicycleComponentV1.setTokenOperatorApproval(tokenId, third.address, true))
+                    .to.emit(bicycleComponentV1, "TokenOperatorApprovalUpdated")
+                    .withArgs(tokenId, third.address, true);
+            });
+
+            it("Should emit TokenOperatorApprovalUpdated event when approval is revoked", async function () {
+                const {bicycleComponentV1, tokenId, third} = await loadFixture(registerComponent);
+
+                await expect(bicycleComponentV1.setTokenOperatorApproval(tokenId, third.address, false))
+                    .to.emit(bicycleComponentV1, "TokenOperatorApprovalUpdated")
+                    .withArgs(tokenId, third.address, false);
+            });
+
+            it("Should not allow setting approval for a non-existing token", async function () {
+                const {bicycleComponentV1, third} = await loadFixture(registerComponent);
+
+                const nonExistingTokenId = ethers.BigNumber.from("1");
+
+                await expect(bicycleComponentV1.setTokenOperatorApproval(nonExistingTokenId, third.address, true))
+                    .to.be.revertedWith("ERC721: invalid token ID");
+            });
+
+            it("Should not allow setting approval for a token not managed by the sender", async function () {
+                const {bicycleComponentV1, tokenId, shop, customer, third} = await loadFixture(registerComponent);
+
+                await expect(bicycleComponentV1.connect(third).setTokenOperatorApproval(tokenId, shop.address, false))
+                    .to.be.revertedWith("Insufficient permissions for approval");
+
+                await expect(bicycleComponentV1.connect(third).setTokenOperatorApproval(tokenId, customer.address, false))
+                    .to.be.revertedWith("Insufficient permissions for approval");
+
+                await expect(bicycleComponentV1.connect(third).setTokenOperatorApproval(tokenId, third.address, true))
+                    .to.be.revertedWith("Insufficient permissions for approval");
+            });
+
+            it("Should allow setting approval to the token owner", async function () {
+                const {bicycleComponentV1, tokenId, customer} = await loadFixture(registerComponent);
+
+                const ownerOf = await bicycleComponentV1.ownerOf(tokenId);
+                expect(ownerOf).to.equal(customer.address);
+
+                const isApprovedBefore = await bicycleComponentV1.tokenOperatorApproval(tokenId, customer.address);
+                expect(isApprovedBefore).to.be.false;
+
+                await expect(bicycleComponentV1.setTokenOperatorApproval(tokenId, customer.address, true))
+                    .to.not.be.reverted;
+
+                const isApprovedAfter = await bicycleComponentV1.tokenOperatorApproval(tokenId, customer.address);
+                expect(isApprovedAfter).to.be.true;
+            });
+
+            it("Should allow the simple owner/customer to manage approval", async function () {
+                const {bicycleComponentV1, tokenId, customer, third} = await loadFixture(registerComponent);
+
+                const ownerOf = await bicycleComponentV1.ownerOf(tokenId);
+                expect(ownerOf).to.equal(customer.address);
+
+                // Customer does not have a special role
+
+                const roles = {
+                    admin: await bicycleComponentV1.DEFAULT_ADMIN_ROLE(),
+                    minter: await bicycleComponentV1.MINTER_ROLE(),
+                    pauser: await bicycleComponentV1.PAUSER_ROLE(),
+                }
+
+                for (const [__, role] of Object.entries(roles)) {
+                    const hasRole = await bicycleComponentV1.hasRole(role, customer.address);
+                    expect(hasRole).to.be.false;
+                }
+
+                // Operator can grant approval
+
+                await bicycleComponentV1.connect(customer).setTokenOperatorApproval(tokenId, third.address, true);
+                expect(await bicycleComponentV1.tokenOperatorApproval(tokenId, third.address)).to.be.true;
+
+                // Operator can revoke approval
+
+                await bicycleComponentV1.connect(customer).setTokenOperatorApproval(tokenId, third.address, false);
+                expect(await bicycleComponentV1.tokenOperatorApproval(tokenId, third.address)).to.be.false;
+
+                // Operator can revoke their own approval
+
+                expect(await bicycleComponentV1.connect(customer).setTokenOperatorApproval(tokenId, customer.address, false))
+                    .not.to.be.reverted;
+
+                const isApprovedAfter = await bicycleComponentV1.tokenOperatorApproval(tokenId, customer.address);
+                expect(isApprovedAfter).to.be.false;
+            });
+
+            it("Should allow the operator to also manage approval for this token", async function () {
+                // Note: This behavior is dubious, and should be reconsidered.
+
+                const {bicycleComponentV1, tokenId, shop, customer, third} = await loadFixture(registerComponent);
+
+                // Customer gives approval to `third`
+                await bicycleComponentV1.connect(customer).setTokenOperatorApproval(tokenId, third.address, true);
+
+                // Before `third` revokes approval, `shop` has approval
+                expect(await bicycleComponentV1.tokenOperatorApproval(tokenId, shop.address)).to.be.true;
+
+                // Revoking approval by `third`
+                await bicycleComponentV1.connect(third).setTokenOperatorApproval(tokenId, shop.address, false);
+
+                // After revoking approval, `shop` does not have approval
+                expect(await bicycleComponentV1.tokenOperatorApproval(tokenId, shop.address)).to.be.false;
+            });
+        });
+
+        describe("isApprovedOrOwner", function () {
             it("Should be true for the current owner of a token", async function () {
                 const {bicycleComponentV1, customer, tokenId} = await loadFixture(registerComponent);
 
                 const isApprovedOrOwner = await bicycleComponentV1.isApprovedOrOwner(customer.address, tokenId);
                 expect(isApprovedOrOwner).to.equal(true);
+            });
+
+            it("Should allow operator to transfer", async function () {
+                const {bicycleComponentV1, customer, third, tokenId} = await loadFixture(registerComponent);
+
+                // todo
+                // await bicycleComponentV1.
+            });
+
+            it("Should disallow third party to transfer", async function () {
+                // todo
+            });
+
+            it("Should revoke approval on transfer", async function () {
+                // todo
+            });
+
+            it("Should grant approval on transfer", async function () {
+                // todo
             });
 
             // todo: more
