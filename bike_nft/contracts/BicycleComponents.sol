4d8c4052f72719ae8e91d20e50e86498963a1a44
RA <numpde@null.net>
2023-05-06 13:17:47 +0400

    separate abstract base

--- bike_nft/contracts/BicycleComponents.sol
+++ bike_nft/contracts/BicycleComponents.sol
@@ -12,17 +12,16 @@ import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 
 /// @title Bicycle Component NFT Contract
 /// @notice This contract stores bicycle components as NFTs.
-contract BicycleComponents is Initializable, ERC721Upgradeable, ERC721EnumerableUpgradeable, ERC721URIStorageUpgradeable, PausableUpgradeable, AccessControlUpgradeable, UUPSUpgradeable {
+abstract contract BicycleComponentsBase is Initializable, ERC721Upgradeable, ERC721EnumerableUpgradeable, ERC721URIStorageUpgradeable, PausableUpgradeable, AccessControlUpgradeable, UUPSUpgradeable {
     bytes32 public constant UPGRADER_ROLE = keccak256("UPGRADER_ROLE");
     bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
-    bytes32 public constant NFT_MANAGER_ROLE = keccak256("NFT_MANAGER_ROLE");
 
     /// @custom:oz-upgrades-unsafe-allow constructor
     constructor() {
         _disableInitializers();
     }
 
-    function initialize() initializer public {
+    function initialize() initializer virtual public {
         __ERC721_init("BicycleComponents", "BICO");
         __ERC721Enumerable_init();
         __ERC721URIStorage_init();
@@ -38,35 +37,6 @@ contract BicycleComponents is Initializable, ERC721Upgradeable, ERC721Enumerable
         _grantRole(PAUSER_ROLE, msg.sender);
     }
 
-    // Allow/disallow the managing contract/address to manage the NFTs of this contract
-
-    function hireManager(address account) public onlyRole(DEFAULT_ADMIN_ROLE) {
-        _grantRole(NFT_MANAGER_ROLE, account);
-    }
-
-    function fireManager(address account) public onlyRole(DEFAULT_ADMIN_ROLE) {
-        _revokeRole(NFT_MANAGER_ROLE, account);
-    }
-
-    /**
-     * @dev Extends the default behavior of `_isApprovedOrOwner` from the inherited ERC721 contract.
-     * In addition to the usual checks, allows `sender` to have the NFT_MANAGER_ROLE.
-     * Moreover, this checks that the token exists using `_requireMinted`.
-     *
-     * @param spender The address to check for approval or ownership.
-     * @param tokenId The token ID to check for the given `spender`.
-     * @return bool True if the `spender` is owner, has approval or has the NFT_MANAGER_ROLE.
-     */
-    function _isApprovedOrOwner(address spender, uint256 tokenId) internal view virtual override returns (bool) {
-        _requireMinted(tokenId);
-        return super._isApprovedOrOwner(spender, tokenId) || hasRole(NFT_MANAGER_ROLE, spender);
-    }
-
-    // Note: This is defined as "external"
-    function isApprovedOrOwner(address spender, uint256 tokenId) external view returns (bool) {
-        return _isApprovedOrOwner(spender, tokenId);
-    }
-
     // ROLE SENTRIES: Pausing the contract
 
     function pause() external onlyRole(PAUSER_ROLE) {
@@ -77,22 +47,6 @@ contract BicycleComponents is Initializable, ERC721Upgradeable, ERC721Enumerable
         _unpause();
     }
 
-    // ROLE SENTRIES: Managing tokens
-
-    function safeMint(address to, uint256 tokenId) external onlyRole(NFT_MANAGER_ROLE) {
-        _safeMint(to, tokenId);
-    }
-
-    function setTokenURI(uint256 tokenId, string memory uri) external {
-        require(_isApprovedOrOwner(msg.sender, tokenId), "Not owner/approved");
-        _setTokenURI(tokenId, uri);
-    }
-
-    function burn(uint256 tokenId) external {
-        require(_isApprovedOrOwner(msg.sender, tokenId), "Not owner/approved");
-        _burn(tokenId);
-    }
-
     // ROLE SENTRIES: Upgrading the contract
 
     // An implementation of `_authorizeUpgrade` is required
@@ -101,12 +55,6 @@ contract BicycleComponents is Initializable, ERC721Upgradeable, ERC721Enumerable
     onlyRole(UPGRADER_ROLE)
     override
     {}
-
-    // Convenience function for token transfer
-
-    function transfer(uint256 tokenId, address to) external {
-        super.safeTransferFrom(ownerOf(tokenId), to, tokenId);
-    }
 
     // The following functions are overrides required by Solidity because:
     // "Two or more base classes define function with same name and parameter types"
@@ -142,5 +90,65 @@ contract BicycleComponents is Initializable, ERC721Upgradeable, ERC721Enumerable
     returns (bool)
     {
         return super.supportsInterface(interfaceId);
+    }
+}
+
+
+contract BicycleComponents is BicycleComponentsBase {
+    bytes32 public constant NFT_MANAGER_ROLE = keccak256("NFT_MANAGER_ROLE");
+
+    // Allow/disallow the managing contract/address to manage the NFTs of this contract
+
+    function hireManager(address account) public onlyRole(DEFAULT_ADMIN_ROLE) {
+        _grantRole(NFT_MANAGER_ROLE, account);
+    }
+
+    function fireManager(address account) public onlyRole(DEFAULT_ADMIN_ROLE) {
+        _revokeRole(NFT_MANAGER_ROLE, account);
+    }
+
+    // Modification of the _isApprovedOrOwner behavior from the inherited ERC721 contract
+
+    /**
+     * @dev Extends the default behavior of `_isApprovedOrOwner` from the inherited ERC721 contract.
+     * In addition to the usual checks, allows `sender` to have the NFT_MANAGER_ROLE.
+     * Moreover, this checks that the token exists using `_requireMinted`.
+     *
+     * @param spender The address to check for approval or ownership.
+     * @param tokenId The token ID to check for the given `spender`.
+     * @return bool True if the `spender` is owner, has approval or has the NFT_MANAGER_ROLE.
+     */
+    function _isApprovedOrOwner(address spender, uint256 tokenId) internal view virtual override returns (bool) {
+        _requireMinted(tokenId);
+        return super._isApprovedOrOwner(spender, tokenId) || hasRole(NFT_MANAGER_ROLE, spender);
+    }
+
+    // Note: This is defined as "external"
+    function isApprovedOrOwner(address spender, uint256 tokenId) external view returns (bool) {
+        return _isApprovedOrOwner(spender, tokenId);
+    }
+
+    // Minting and burning
+
+    function safeMint(address to, uint256 tokenId) external onlyRole(NFT_MANAGER_ROLE) {
+        _safeMint(to, tokenId);
+    }
+
+    function burn(uint256 tokenId) external {
+        require(_isApprovedOrOwner(msg.sender, tokenId), "Not owner/approved");
+        _burn(tokenId);
+    }
+
+    // Re-setting token URI
+
+    function setTokenURI(uint256 tokenId, string memory uri) external {
+        require(_isApprovedOrOwner(msg.sender, tokenId), "Not owner/approved");
+        _setTokenURI(tokenId, uri);
+    }
+
+    // Convenience function for token transfer
+
+    function transfer(uint256 tokenId, address to) external {
+        super.safeTransferFrom(ownerOf(tokenId), to, tokenId);
     }
 }
