b3c4466f3056efd73d94e2f45e1dd4fafd5aafd4
RA <numpde@null.net>
2023-05-18 14:58:02 +0400

    iteration

--- bike_nft/data/make_blanks_nft_image.py
+++ bike_nft/data/make_blanks_nft_image.py
@@ -1,14 +1,37 @@
+import json
+
 from pathlib import Path
 
+from PIL.Image import Image
 from bugs import mkdir
 
-from make_blanks_collection_image import make_and_upload_image
+from make_blanks_collection_image import make_image_plain_text
+from infura import send_to_infura
+
+
+def image_to_buffer(image: Image):
+    from io import BytesIO
+    buffer = BytesIO()
+    image.save(buffer, format="JPEG")
+    buffer.seek(0)
+    return buffer
 
 
 def main():
-    text = "\n".join(["On-chain", "bicycle registration", "BLANK NFT"])
+    texts = {
+        authority: "\n".join(["On-chain", "bicycle registration", "BLANK NFT", authority])
+        for authority in ["A", "B", "C", "D", "--"]
+    }
 
-    make_and_upload_image(text, local_path=mkdir(Path(__file__).with_suffix('')))
+    images = {
+        k: image_to_buffer(make_image_plain_text(text))
+        for (k, text) in texts.items()
+    }
+
+    manifest = send_to_infura(pin=True, files=images)
+
+    with (mkdir(Path(__file__).with_suffix('')) / "manifest.json").open(mode='w') as fd:
+        json.dump(manifest, fd, indent=2)
 
 
 if __name__ == '__main__':
